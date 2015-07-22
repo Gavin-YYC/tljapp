875e10eb494fb9d924e67549620515e226b28461
Gavin <q414625852@163.com>
2015-07-22 20:48:23 +0800

    兼职列表页下拉刷新，提示

--- js/controllers.js
+++ js/controllers.js
@@ -1,28 +1,62 @@
+/*
+ *
+ * list Controller （兼职列表页）
+ * api：http://120.24.218.56/api/job/list
+ * test api : http://localhost:8100/js/data.js
+ *
+*/
 angular.module('starter.controllers',[])
-.controller('ListController',function($scope,$http){
-        $scope.items = [
-            {type:1,title:"你好倒萨这时测试ְ",gongzi:'20/天',location:"张店",date:'2015-1-28:18:00',gztype:"日结"},
-            {type:1,title:"你好倒萨这时测试",gongzi:'200/天',location:"张店",date:'2015-1-28:18:00',gztype:"日结"},
-            {type:1,title:"sfdfdsfsdfsd",gongzi:'2000/天',location:"张店",date:'2015-1-28:18:00',gztype:"日结"}
-        ];
-        $scope.doRefresh = function() {
-            for(var i = 0;i<10;i++,base.type++){
-                var base = {type:base,id:Math.ceil(Math.random(100,999)*100),title:"sfdfdsfsdfsd"+i,gongzi:'2000/天',location:"张店",date:'2015-1-28:18:00',gztype:"日结"};
-                $scope.items.unshift(base);
-                console.log(base);
+.controller('ListController',function($scope,$http,$ionicPopup, $timeout){
+
+    var pageBase = 0;
+    var pageSize = 8;
+    var listApi = "http://120.24.218.56/api/job/list";
+    
+    $http.get(listApi)
+        .success(function(newItems) {
+            if (newItems.ok == true) {
+                $scope.items = newItems.data;
+                console.log(newItems.ok);
+            }else{
+                //else code
+            };
+        })
+        .finally(function() {
+            $scope.$broadcast('scroll.refreshComplete');
+        });
+
+    $scope.doRefresh = function(){
+        pageBase++;
+        $http.get(listApi+"?pageNumber="+pageBase+"&pageSize="+pageSize)
+            .success(function(newItems) {
+                if (newItems.ok == true && newItems.data.length > 0) {
+                    for (var i = 0; i < newItems.data.length; i++) {
+                        $scope.items.unshift(newItems.data[i]);
+                    }; 
+                    var alertPopup = $ionicPopup.alert({
+                        title: '友情提示：',
+                        template: '刷新成功！'
+                    });
+                }else if(newItems.ok == true && newItems.data.length == 0) {
+                    var alertPopup = $ionicPopup.alert({
+                        title: '友情提示：',
+                        template: '已经没有更多信息了( ¯ □ ¯ )'
+                    });
+                }else{
+                    var alertPopup = $ionicPopup.alert({
+                        title: '友情提示：',
+                        template: newItems.message
+                    });
+                };
+            })
+            .finally(function() {
                 $scope.$broadcast('scroll.refreshComplete');
-            }
-            /*
-            $http.get('#')
-                .success(function(newItems) {
-                    $scope.items = newItems;
-                })
-                .finally(function() {
-                    $scope.$broadcast('scroll.refreshComplete');
-                });
-            */
-        };
+            });
+    }
+
 })
+
+
 .controller('DetailController',function($scope,$http){
     $scope.nihao = "nihao";
  })

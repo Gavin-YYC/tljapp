19dc97e054b67b890c2573b00347ba924c557055
Gavin <q414625852@163.com>
2015-07-24 00:24:08 +0800

    二级页面分类跳转完成

--- js/controllers.js
+++ js/controllers.js
@@ -6,17 +6,151 @@
  *
 */
 angular.module('starter.controllers',[])
-.controller('ListController',function($scope,$http,$ionicPopup, $timeout){
+.controller('ListController',function($scope,$http,$ionicPopup, $timeout,$ionicModal,$stateParams){
 
+    //初始化 请求页面参数
     var pageBase = 0;
     var pageSize = 8;
+    var cateId = $stateParams.id;
     var listApi = "http://120.24.218.56/api/job/list";
+    var cateListApi = "http://120.24.218.56/api/job/category/"+cateId;
+
+    if (cateId == 0) {
+        //列表页加载时产生的数据
+        $http.get(listApi)
+            .success(function(newItems) {
+                if (newItems.ok == true) {
+                    $scope.items = newItems.data;
+                }else{
+                    //else code
+                };
+            })
+            .finally(function() {
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+    }else{
+        $http.get(cateListApi)
+            .success(function(newItems) {
+                if (newItems.ok == true) {
+                    $scope.items = newItems.data;
+                }else{
+                    //else code
+                };
+            })
+            .finally(function() {
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+    };
     
-    $http.get(listApi)
+
+
+    //下拉更新操作，数据更新，并进行提示
+    $scope.doRefresh = function(){
+        if (cateId==0) {
+            pageBase++;
+            $http.get(listApi+"?pageNumber="+pageBase+"&pageSize="+pageSize)
+                .success(function(newItems) {
+                    if (newItems.ok == true && newItems.data.length > 0) {
+                        for (var i = 0; i < newItems.data.length; i++) {
+                            $scope.items.unshift(newItems.data[i]);
+                        }; 
+                        var alertPopup = $ionicPopup.alert({
+                            title: '友情提示：',
+                            template: '刷新成功！'
+                        });
+                    }else if(newItems.ok == true && newItems.data.length == 0) {
+                        var alertPopup = $ionicPopup.alert({
+                            title: '友情提示：',
+                            template: '已经没有更多信息了( ¯ □ ¯ )'
+                        });
+                    }else{
+                        var alertPopup = $ionicPopup.alert({
+                            title: '友情提示：',
+                            template: newItems.message
+                        });
+                    };
+                })
+                .finally(function() {
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+        }else{
+            pageBase++;
+            $http.get(cateListApi+"?pageNumber="+pageBase+"&pageSize="+pageSize)
+                .success(function(newItems) {
+                    if (newItems.ok == true && newItems.data.length > 0) {
+                        for (var i = 0; i < newItems.data.length; i++) {
+                            $scope.items.unshift(newItems.data[i]);
+                        }; 
+                        var alertPopup = $ionicPopup.alert({
+                            title: '友情提示：',
+                            template: '刷新成功！'
+                        });
+                    }else if(newItems.ok == true && newItems.data.length == 0) {
+                        var alertPopup = $ionicPopup.alert({
+                            title: '友情提示：',
+                            template: '已经没有更多信息了( ¯ □ ¯ )'
+                        });
+                    }else{
+                        var alertPopup = $ionicPopup.alert({
+                            title: '友情提示：',
+                            template: newItems.message
+                        });
+                    };
+                })
+                .finally(function() {
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+        };
+    }
+
+    //modal弹出模态窗口，用于选择分类、结算和区域
+    $ionicModal.fromTemplateUrl('modal.html',function(modal){
+        $scope.modal = modal;
+    },{
+        animation: 'slide-in-up'
+    })
+
+})
+
+/*
+ *
+ * Detail Controller （兼职详情页）
+ * api：http://120.24.218.56/api/job/
+ * test api : http://localhost:8100/js/detail.json
+ *
+*/
+.controller('DetailController',function($scope,$http,$stateParams){
+    $scope.id = $stateParams.id;
+    var jobApi = "http://120.24.218.56/api/job/";
+
+    $http.get("http://localhost:8100/js/detail.json")
+        .success(function(jobDetail){
+            if (jobDetail.ok == true) {
+                $scope.item = jobDetail.data;
+                console.log($scope.item);
+            };
+        })
+        .finally(function() {
+            $scope.$broadcast('scroll.refreshComplete');
+        });
+ })
+
+/*
+ *
+ * Detail Controller （兼职详情页）
+ * api：http://120.24.218.56/api/job/cate/list
+ * test api : http://localhost:8100/js/category.json
+ *
+*/
+.controller('getCategoriesCotroller',function($scope,$http){
+
+    var categoryApi = "http://120.24.218.56/api/job/cate/list";
+
+    $http.get(categoryApi)
         .success(function(newItems) {
             if (newItems.ok == true) {
-                $scope.items = newItems.data;
-                console.log(newItems.ok);
+                $scope.categories = newItems.data;
+                console.log($scope.categories);
             }else{
                 //else code
             };
@@ -25,38 +159,7 @@ angular.module('starter.controllers',[])
             $scope.$broadcast('scroll.refreshComplete');
         });
 
-    $scope.doRefresh = function(){
-        pageBase++;
-        $http.get(listApi+"?pageNumber="+pageBase+"&pageSize="+pageSize)
-            .success(function(newItems) {
-                if (newItems.ok == true && newItems.data.length > 0) {
-                    for (var i = 0; i < newItems.data.length; i++) {
-                        $scope.items.unshift(newItems.data[i]);
-                    }; 
-                    var alertPopup = $ionicPopup.alert({
-                        title: '友情提示：',
-                        template: '刷新成功！'
-                    });
-                }else if(newItems.ok == true && newItems.data.length == 0) {
-                    var alertPopup = $ionicPopup.alert({
-                        title: '友情提示：',
-                        template: '已经没有更多信息了( ¯ □ ¯ )'
-                    });
-                }else{
-                    var alertPopup = $ionicPopup.alert({
-                        title: '友情提示：',
-                        template: newItems.message
-                    });
-                };
-            })
-            .finally(function() {
-                $scope.$broadcast('scroll.refreshComplete');
-            });
+    $scope.goList = function(id){
+        console.log(id);
     }
-
 })
-
-
-.controller('DetailController',function($scope,$http){
-    $scope.nihao = "nihao";
- })
--- js/route.js
+++ js/route.js
@@ -6,7 +6,7 @@ angular.module('route',[])
                 templateUrl:'templates/index.html'
             })
             .state('list', {
-                url:'/list',
+                url:'/list/:id',
                 templateUrl:'templates/list.html',
                 controller:'ListController'
             })
